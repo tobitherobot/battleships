16e1399d4f59fd06f5246cfcf062b493ac879477
BrozartTheGlockSaint <108093212+BrozartTheGlockSaint@users.noreply.github.com>
2023-01-14 15:19:57 +0100

    minor changes

--- Field.cpp
+++ Field.cpp
@@ -4,6 +4,7 @@
 
 Field::Field() {
     shipComponents = 17;
+    third = false;
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             field[i][j] = '~';
@@ -50,24 +51,125 @@ void Field::printFogField() {
 };
 
 void Field::shoot(Field &enemy) {
-    std::string coordinates;
-    int first;
-    int second;
+    bool incomplete = true;
+    while(incomplete) {
+        std::string coordinates;
+        int first;
+        int second;
+        std::cout << "Enter coordinates to shoot at. Example: A7" << std::endl;
+        std::cin >> coordinates;
+        first = charToInt(coordinates.at(0));
+        second = int(coordinates.at(1) - 48);
+        std::cout << first << second << std::endl;
 
-    std::cout << "Enter coordinates to shoot at. Example: A7" << std::endl;
-    std::cin >> coordinates;
-    first = charToInt(coordinates.at(0));
-    second = int(coordinates.at(1) - 48);
-
-    if (enemy.checkPos(first,second) == 'O') {
-        std::cout << "Hit!" << std::endl;
-        enemy.setPos(first, second, 'X');
-        enemy.reduceShipComponents();
-    } else {
-        std::cout << "Miss!" << std::endl;
-        enemy.setPos(first, second, 'M');
+        if (enemy.checkPos(first, second) == 'O') {
+            std::cout << "Hit!" << std::endl;
+            enemy.setPos(first, second, 'X');
+            removeHitComponent(enemy ,first, second);
+            enemy.reduceShipComponents();
+            incomplete = false;
+        } else {
+            std::cout << "Miss!" << std::endl;
+            enemy.setPos(first, second, 'M');
+            incomplete = false;
+        }
     }
+    
 };
+
+void Field::removeHitComponent(Field &enemy, int first, int second) {
+    bool ship5destroyed = true;
+    bool ship4destroyed = true;
+    bool ship3destroyed = true;
+    bool ship32destroyed = true;
+    bool ship2destroyed = true;
+
+    for (int i = 0; i < 5; i++) {
+        if (first == enemy.ship5[i][0] && second == enemy.ship5[i][1]) {
+            enemy.ship5[i][0] = 66;
+            enemy.ship5[i][1] = 66;
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        if (first == enemy.ship4[i][0] && second == enemy.ship4[i][1]) {
+            enemy.ship4[i][0] = 66;
+            enemy.ship4[i][1] = 66;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        if (first == enemy.ship3[i][0] && second == enemy.ship3[i][1]) {
+            enemy.ship3[i][0] = 66;
+            enemy.ship3[i][1] = 66;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        if (first == enemy.ship32[i][0] && second == enemy.ship32[i][1]) {
+            enemy.ship32[i][0] = 66;
+            enemy.ship32[i][1] = 66;
+        }
+    }
+    for (int i = 0; i < 2; i++) {
+        if (first == enemy.ship2[i][0] && second == enemy.ship2[i][1]) {
+            enemy.ship2[i][0] = 66;
+            enemy.ship2[i][1] = 66;
+        }
+    }
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (enemy.ship5[i][j] != 66) {
+                ship5destroyed = false;
+            }
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (enemy.ship5[i][j] != 66) {
+                ship4destroyed = false;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (enemy.ship5[i][j] != 66) {
+                ship3destroyed = false;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (enemy.ship5[i][j] != 66) {
+                ship32destroyed = false;
+            }
+        }
+    }
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (enemy.ship5[i][j] != 66) {
+                ship2destroyed = false;
+            }
+        }
+    }
+    if (ship5destroyed) {
+        std::cout << "You destroyed a ship with length 5!" << std::endl;
+        enemy.ship5[0][0] = 99;
+    }
+    if (ship4destroyed) {
+        std::cout << "You destroyed a ship with length 4!" << std::endl;
+        enemy.ship4[0][0] = 99;
+    }
+    if (ship3destroyed) {
+        std::cout << "You destroyed a ship with length 3!" << std::endl;
+        enemy.ship3[0][0] = 99;
+    }
+    if (ship32destroyed) {
+        std::cout << "You destroyed a ship with length 3!" << std::endl;
+        enemy.ship32[0][0] = 99;
+    }
+    if (ship2destroyed) {
+        std::cout << "You destroyed a ship with length 2!" << std::endl;
+        enemy.ship2[0][0] = 99;
+    }
+}
 
 void Field::setPos(int first, int second, char symbol) {
     field[first - 1][second - 1] = symbol;
@@ -109,7 +211,18 @@ void Field::placeShip(int len) {
         secondPosY = charToInt(input.at(3));
         secondPosX = int(input.at(4) - 48);
 
-        std::cout << firstPosX << firstPosY << secondPosX << secondPosY << std::endl;
+        std::cout << firstPosY << firstPosX << secondPosY << secondPosX << std::endl;
+
+        if (firstPosY < 0 || firstPosY > 9 || firstPosX < 0 || firstPosX > 9 || secondPosY < 0 || secondPosY > 9 || secondPosX < 0 || secondPosX > 9) {
+            std::cout << "Your input " << input << " is incorrect! Please try again!" << std::endl;
+            continue;
+        } else if (firstPosY != secondPosY && firstPosX != secondPosX) {
+            std::cout << "Your coordinates " << input << " are not in one row/column! Please try again!" << std::endl;
+            continue;
+        } else if (firstPosY - secondPosY != 0 && abs(firstPosX - secondPosX) + 1 != len || firstPosY - secondPosY == 0 && abs(firstPosX - secondPosX) + 1 != len) {
+            std::cout << "Your input " << input << " is not length " << len << "! Please try again!" << std::endl;
+            continue;
+        }
 
         if (firstPosY - secondPosY != 0) {
             //Fall, dass das Schiff in X-Richtung platziert wurde
@@ -121,7 +234,31 @@ void Field::placeShip(int len) {
             }
             for (int i = 0; i < len; i++) {
                 field[firstPosY - 1 + i][firstPosX - 1] = 'O';
+                switch(len) {
+                    case 5:
+                        ship5[i][0] = firstPosY - 1 + i;
+                        ship5[i][1] = firstPosX - 1;
+                        break;
+                    case 4:
+                        ship4[i][0] = firstPosY - 1 + i;
+                        ship4[i][1] = firstPosX - 1;
+                        break;
+                    case 3:
+                        if (!third) {
+                            ship3[i][0] = firstPosY - 1 + i;
+                            ship3[i][1] = firstPosX - 1;
+                        } else {
+                            ship32[i][0] = firstPosY - 1 + i;
+                            ship32[i][1] = firstPosX - 1;
+                        }
+                        break;
+                    case 2:
+                        ship2[i][0] = firstPosY - 1 + i;
+                        ship2[i][1] = firstPosX - 1;
+                    break;
+                }
             }
+
         } else {
             //Fall, dass das Schiff in Y-Richtung platziert wurde
             for (int i = 0; i < len; i++) {
@@ -132,6 +269,29 @@ void Field::placeShip(int len) {
             }
             for (int i = 0; i < len; i++) {
                 field[firstPosY - 1][firstPosX - 1 + i] = 'O';
+                switch(len) {
+                    case 5:
+                        ship5[i][0] = firstPosY - 1;
+                        ship5[i][1] = firstPosX - 1 + i;
+                        break;
+                    case 4:
+                        ship4[i][0] = firstPosY - 1;
+                        ship4[i][1] = firstPosX - 1 + i;
+                        break;
+                    case 3:
+                        if (!third) {
+                            ship3[i][0] = firstPosY - 1;
+                            ship3[i][1] = firstPosX - 1 + i;
+                        } else {
+                            ship32[i][0] = firstPosY - 1;
+                            ship32[i][1] = firstPosX - 1 + i;
+                        }
+                        break;
+                    case 2:
+                        ship2[i][0] = firstPosY - 1;
+                        ship2[i][1] = firstPosX - 1 + i;
+                    break;
+                }
             }
         }
         incomplete = false;
--- Field.h
+++ Field.h
@@ -14,11 +14,18 @@ class Field {
     void shoot(Field &enemy);
     void reduceShipComponents();
     int getShipComponents();
+    void removeHitComponent(Field &enemy, int, int);
+
+    bool third;
+    int ship5[5][2];
+    int ship4[4][2];
+    int ship3[3][2];
+    int ship32[3][2];
+    int ship2[2][2];
 
     private:
     char field[10][10];
     int shipComponents;
-
 };
 
 #endif
